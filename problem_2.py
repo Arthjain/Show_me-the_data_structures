a811c2e771b96a0975870311086638e094d9d407
Arth jain <arthjainabc@gmail.com>
2020-06-02 05:40:45 +0530

    Update problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -1,41 +1,27 @@
-
 import os
 
 
-def main():
-    """Main function call to test the find_files function."""
-    files = find_files(".c", "testdir")
-    print(files)
-    assert files == [
-        "testdir/subdir5/a.c",
-        "testdir/subdir3/subsubdir1/b.c",
-        "testdir/subdir1/a.c",
-        "testdir/t1.c",
-    ]
+def find_using_loop(suffix, path):
+    files = []
+    paths = []
+    paths.append(path)
 
-    print("All test cases passed!")
+    while len(paths) > 0:
+        path = paths.pop()
+        if os.path.isfile(path):
+            if path.endswith(suffix):
+                files.append(path)
+        else:
+            new_paths =  os.listdir(path)
+            for item in new_paths:
+                paths.append("{}/{}".format(path, item))
 
+    return files
 
-def find_files(suffix, path):
-    """Find all files beneath path with file name suffix.
-    Args:
-        suffix: A str representing the suffix of the file name to be found
-        path: A str representing the path of the file system
-    Returns:
-        file_list: A list of strs representing paths of files with the given
-            suffix
-    """
-    file_list = []
+print(find_using_loop('.c', '.'))
+print('\n')
 
-    for entry in os.listdir(path):
-        entry_path = os.path.join(path, entry)
-        if os.path.isfile(entry_path) and entry.endswith(suffix):
-            file_list.append(entry_path)
-        if os.path.isdir(entry_path):
-            file_list += find_files(suffix, entry_path)
+print(find_using_loop('', '.'))
+print('\n')
 
-    return file_list
-
-
-if __name__ == "__main__":
-    main()
+print(find_using_loop('.h', '.'))
